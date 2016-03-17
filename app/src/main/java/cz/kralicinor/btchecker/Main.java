57e26ef7f1a0a924f0da9aad010b96b52d60e5e2
Martin Babič <martin.babic@gisat.cz>
2016-03-17 17:02:15 +0100

    Backup commit

--- app/src/main/java/cz/kralicinor/btchecker/Main.java
+++ app/src/main/java/cz/kralicinor/btchecker/Main.java
@@ -2,20 +2,27 @@ package cz.kralicinor.btchecker;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.EditText;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
 
 public class Main extends AppCompatActivity {
 
-    private byte[] runs;
+    private EditText console;
+    private BroadcastReceiver receiver;
+    private BluetoothAdapter bluetoothAdapter;
+    private BluetoothDevice bluetoothDevice;
+    private String deviceAddress;
+    private UUID uuid = UUID.fromString( "2c15c8b2-ec47-11e5-9ce9-5e5517507c66" );
     private Handler handler;
     private Runnable runnable;
 
@@ -24,57 +31,77 @@ public class Main extends AppCompatActivity {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.main );
 
-        final ListView pairedDevicesList = ( ListView ) findViewById( R.id.mainPairedDevicesList );
-        final TextView nextCheck = ( TextView ) findViewById( R.id.mainPairedDevicesNextCheck );
+        console = ( EditText ) findViewById( R.id.mainPairedDevicesList );
 
-        final BluetoothAdapter bluetoothAdapter;
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-
-        if ( !bluetoothAdapter.isEnabled() ) {
-            bluetoothAdapter.enable();
-            Toast.makeText( this, "Enabling bluetooth...", Toast.LENGTH_SHORT ).show();
+        String pairedDevices = "Paired:";
+        for ( BluetoothDevice device : bluetoothAdapter.getBondedDevices() ) {
+            if ( device.getName().startsWith( "MI1" ) ) {
+                bluetoothDevice = device;
+                deviceAddress = device.getAddress();
+            }
+            pairedDevices += " " + device.getName() + "[" + device.getAddress() + "]";
         }
+        printToConsole( pairedDevices );
 
-        runs = new byte[]{ 0 };
         handler = new Handler();
-
         runnable = new Runnable() {
             @Override
             public void run() {
-                try {
-                    if ( runs[ 0 ] == 0 || runs[ 0 ] >= 15 ) {
-                        List< String > btDevNames = new ArrayList<>();
-                        for ( BluetoothDevice device : bluetoothAdapter.getBondedDevices() ) {
-                            if ( device.getBondState() == BluetoothDevice.BOND_BONDED ) {
-                                btDevNames.add( device.getName() );
-                            }
+                new Thread( new Runnable() {
+                    @Override
+                    public void run() {
+                        if ( bluetoothDevice != null ) {
+                            printToConsole( String.valueOf( bluetoothDevice.fetchUuidsWithSdp() ) );
                         }
-                        final ArrayAdapter< String > arrayAdapter = new ArrayAdapter<>( getApplicationContext(), R.layout.main_devices_list_item, btDevNames );
-                        if ( pairedDevicesList != null ) {
-                            pairedDevicesList.post( new Runnable() {
-                                @Override
-                                public void run() {
-                                    pairedDevicesList.setAdapter( arrayAdapter );
-                                }
-                            } );
+                        else {
+                            printToConsole( "Not found!" );
                         }
-                        runs[ 0 ] = 0;
+                        handler.postDelayed( this, 30000 );
                     }
-                    if ( nextCheck != null ) {
-                        nextCheck.post( new Runnable() {
-                            @Override
-                            public void run() {
-                                nextCheck.setText( String.valueOf( runs[ 0 ] ) );
-                            }
-                        } );
-                    }
+                } ).start();
+            }
+        };
+        handler.postDelayed( runnable, 10000 );
+
+        receiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive( Context context, Intent intent ) {
+                String action = intent.getAction();
+                if ( action.equalsIgnoreCase( BluetoothDevice.ACTION_ACL_CONNECTED ) ) {
+                    BluetoothDevice device = intent.getParcelableExtra( BluetoothDevice.EXTRA_DEVICE );
+                    printToConsole( "BT+" + device.getName() );
                 }
-                finally {
-                    runs[ 0 ]++;
-                    handler.postDelayed( this, 1000 );
+                else if ( action.equalsIgnoreCase( BluetoothDevice.ACTION_ACL_DISCONNECTED ) ) {
+                    BluetoothDevice device = intent.getParcelableExtra( BluetoothDevice.EXTRA_DEVICE );
+                    printToConsole( "BT-" + device.getName() );
                 }
             }
         };
-        runnable.run();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver( receiver, new IntentFilter( BluetoothDevice.ACTION_ACL_CONNECTED ) );
+        registerReceiver( receiver, new IntentFilter( BluetoothDevice.ACTION_ACL_DISCONNECTED ) );
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver( receiver );
+    }
+
+    private void printToConsole( final String string ) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat( "y/MM/dd - HH:mm:ss" );
+        final String timestamp = simpleDateFormat.format( new Date() );
+        final String consoleText = console.getText().toString();
+        console.post( new Runnable() {
+            @Override
+            public void run() {
+                console.setText( timestamp + " | " + string + "\n" + consoleText );
+            }
+        } );
     }
 }
