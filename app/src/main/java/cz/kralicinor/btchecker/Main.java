77df66a3c6ff47ac8462f9b39cbe63341940e26a
kralicinora <kralicinora@gmail.com>
2016-03-17 01:09:32 +0100

    Event receiver to check bt device state

--- app/src/main/java/cz/kralicinor/btchecker/Main.java
+++ app/src/main/java/cz/kralicinor/btchecker/Main.java
@@ -1,80 +1,63 @@
 package cz.kralicinor.btchecker;
 
-import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.EditText;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Main extends AppCompatActivity {
 
-    private byte[] runs;
-    private Handler handler;
-    private Runnable runnable;
+    private EditText console;
+    private BroadcastReceiver receiver;
 
     @Override
-    protected void onCreate( Bundle savedInstanceState ) {
-        super.onCreate( savedInstanceState );
-        setContentView( R.layout.main );
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
 
-        final ListView pairedDevicesList = ( ListView ) findViewById( R.id.mainPairedDevicesList );
-        final TextView nextCheck = ( TextView ) findViewById( R.id.mainPairedDevicesNextCheck );
+        console = (EditText) findViewById(R.id.mainPairedDevicesList);
 
-        final BluetoothAdapter bluetoothAdapter;
-        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        printToConsole("Receivers are registered...");
 
-        if ( !bluetoothAdapter.isEnabled() ) {
-            bluetoothAdapter.enable();
-            Toast.makeText( this, "Enabling bluetooth...", Toast.LENGTH_SHORT ).show();
-        }
-
-        runs = new byte[]{ 0 };
-        handler = new Handler();
-
-        runnable = new Runnable() {
+        receiver = new BroadcastReceiver() {
             @Override
-            public void run() {
-                try {
-                    if ( runs[ 0 ] == 0 || runs[ 0 ] >= 15 ) {
-                        List< String > btDevNames = new ArrayList<>();
-                        for ( BluetoothDevice device : bluetoothAdapter.getBondedDevices() ) {
-                            if ( device.getBondState() == BluetoothDevice.BOND_BONDED ) {
-                                btDevNames.add( device.getName() );
-                            }
-                        }
-                        final ArrayAdapter< String > arrayAdapter = new ArrayAdapter<>( getApplicationContext(), R.layout.main_devices_list_item, btDevNames );
-                        if ( pairedDevicesList != null ) {
-                            pairedDevicesList.post( new Runnable() {
-                                @Override
-                                public void run() {
-                                    pairedDevicesList.setAdapter( arrayAdapter );
-                                }
-                            } );
-                        }
-                        runs[ 0 ] = 0;
-                    }
-                    if ( nextCheck != null ) {
-                        nextCheck.post( new Runnable() {
-                            @Override
-                            public void run() {
-                                nextCheck.setText( String.valueOf( runs[ 0 ] ) );
-                            }
-                        } );
-                    }
-                }
-                finally {
-                    runs[ 0 ]++;
-                    handler.postDelayed( this, 1000 );
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+                if (action.equalsIgnoreCase(BluetoothDevice.ACTION_ACL_CONNECTED)) {
+                    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                    printToConsole("BT+" + device.getName());
+                } else if (action.equalsIgnoreCase(BluetoothDevice.ACTION_ACL_DISCONNECTED)) {
+                    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                    printToConsole("BT-" + device.getName());
                 }
             }
         };
-        runnable.run();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(receiver, new IntentFilter(BluetoothDevice.ACTION_ACL_CONNECTED));
+        registerReceiver(receiver, new IntentFilter(BluetoothDevice.ACTION_ACL_DISCONNECTED));
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(receiver);
+    }
+
+    private void printToConsole(String string) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("y/MM/dd - HH:mm:ss");
+        String timestamp = simpleDateFormat.format(new Date());
+        String consoleText = console.getText().toString();
+        console.setText(timestamp + " | " + string + "\n" + consoleText);
     }
 }
